e55d3a40e55f4f87a80a853b7acc6a6fc7b4003a
Yuriy Yunikov <yy@vgs.io>
2021-11-30 18:57:22 +0200

    Postico

--- init/mac/40_curl_installations.sh
+++ init/mac/40_curl_installations.sh
@@ -5,4 +5,23 @@ echo "Installing apps from cURL."
 # sdkman
 curl -s "https://get.sdkman.io?rcupdate=false" | bash
 
+download_and_install_dmg() {
+	curl -s -L "$1" --output tmp/$2.dmg
+	sudo hdiutil attach tmp/$2.dmg
+	# installer doesn't work since can't easily find out Volume name
+	# will need to detach manually in the meantime
+
+	# sudo installer -package /Volumes/$2/$2.pkg -target /
+	# sudo hdiutil detach tmp/$2.dmg
+}
+
+# temp dir
+mkdir tmp
+
+# Postico 2
+download_and_install_dmg https://releases.eggerapps.at/postico2/download postico
+
+# remove temp dir
+rm -rf tmp
+
 echo "DONE Installing apps from cURL."
